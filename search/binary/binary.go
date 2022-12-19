18ff6708fc123e0afcb709f3d2b27c6496b4e98b
GreenPine <x@oruby.cn>
2022-12-19 13:18:22 +0800

    add binary search: strict & similar search

---
+++ search/binary/binary.go
@@ -0,0 +1,45 @@
+package binary
+
+import (
+	"github.com/quxiaolong1504/Algorithms/constraints"
+)
+
+// Search 精确查找
+func Search[T constraints.Ordered](arr []T, k T) int {
+	var l, r = 0, len(arr) - 1
+	for l <= r {
+		var mid = (l + r) / 2 // 避免 int 溢出
+		if arr[mid] == k {
+			return mid
+		} else if k < arr[mid] {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	return -1
+}
+
+// SimilarSearch 近似查找, Closet to T
+func SimilarSearch[T constraints.Number](arr []T, k T) int {
+	var l, r = 0, len(arr) - 1
+
+	// 最后留两个元素
+	for l < r-1 {
+		var mid = (l + r) / 2
+		if k < arr[mid] {
+			r = mid
+		} else {
+			l = mid
+		}
+	}
+
+	if k-arr[l] < arr[r]-k {
+		// 左边更接近 k
+		return l
+	} else {
+		// 右边更接近 k
+		return r
+	}
+
+}
---
+++ search/binary/binary_test.go
@@ -0,0 +1,21 @@
+package binary
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBinarySearch(t *testing.T) {
+	var arr = []int{1, 2, 3, 4, 5, 6, 7}
+	assert.Equal(t, Search[int](arr, 5), 4)
+	assert.Equal(t, Search[int](arr, 1), 0)
+	assert.Equal(t, Search[int](arr, 0), -1)
+}
+
+func TestSimilarSearch(t *testing.T) {
+	var arr = []int{1, 10, 20, 32, 40, 48, 90}
+
+	assert.Equal(t, SimilarSearch[int](arr, 30), 3)
+	assert.Equal(t, SimilarSearch[int](arr, 11), 1)
+	assert.Equal(t, SimilarSearch[int](arr, 48), 5)
+}
